173af4fc2e742936bbbd490bf3e48544f62fc079
tryingfly <yin123yyp@163.com>
2019-07-02 21:04:55 +0800

    自瞄预测release

--- Src/gpio.c
+++ Src/gpio.c
@@ -98,7 +98,7 @@ void MX_GPIO_Init(void)
   /*Configure GPIO pins : PEPin PEPin PEPin PEPin */
   GPIO_InitStruct.Pin = leftSensor_Pin|rightSensor_Pin|leftSwitch_Pin|rightSwitch_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
   HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
 
   /*Configure GPIO pins : PFPin PFPin */
